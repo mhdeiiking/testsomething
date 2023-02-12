991f53877d1ccb31e1e1764da190b9c2dda5a9cf
Mahdi Diaa <69827418+mhdeiiking@users.noreply.github.com>
2023-02-12 03:15:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,47 @@
 from bs4 import BeautifulSoup
 import lxml,json,requests
 from flask import *
+import random
+import string
+import asSQL
+from asSQL import Client
+from flask import Flask, request, jsonify
+users_db = Client("users")
+users_table = users_db['users']
+users_table.create_table()
 
+bots_db = Client("bots")
+bots_table = bots_db['bots']
+bots_table.create_table()
 app = Flask(__name__)
+@app.route('/create_bot', methods=['POST'])
+def create_bot():
+    bot_username = request.json.get('bot_username')
+    bot_name = request.json.get('bot_name')
+    user_id = request.json.get('user_id')
+
+    if bots_table.key_exists(bot_username):
+        return jsonify({"result": "Username already taken"}), 400
+    
+    token = ''.join(random.choices(string.ascii_letters + string.digits, k=30))
+    bot_info = {"bot_name": bot_name, "bot_username": bot_username, "token": token}
+
+    bots_table.set(token, bot_info)
+    if users_table.key_exists(user_id):
+        users_table.push(user_id, bot_info)
+    else:
+        users_table.set(user_id, [bot_info])
+    return jsonify({"result": bot_info}), 200
+
+@app.route('/bot<token>/getme', methods=['GET'])
+def getme(token):
+    
+    if not bots_table.key_exists(token):
+        return jsonify({"result": "Unauthorized"}), 401
+    
+    bot_info = bots_table.get(token)
+    return jsonify({"result": bot_info}), 200
+
 @app.route("/data/")
 def info():
     userid = request.args.get("id")
